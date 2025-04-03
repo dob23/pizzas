2f9cb724416aaad957dd826113fb07e96a1cfa08
Daniel Ortiz <danbedoya2001@gmail.com>
2025-04-03 09:05:07 -0500

    tabla pizza_ingredients

--- database/migrations/2025_04_03_140318_create_pizza_ingredients_table.php
+++ database/migrations/2025_04_03_140318_create_pizza_ingredients_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ingredients', function (Blueprint $table) {
+        Schema::create('pizza_ingredients', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 255);
+            $table->foreignId('pizza_id')->constrained('pizzas')->onDelete('cascade');
+            $table->foreignId('ingredient_id')->constrained('ingredients')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ingredients');
+        Schema::dropIfExists('pizza_ingredients');
     }
 };
