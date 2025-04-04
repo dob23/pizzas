4f4033d9cc4d20f28c24d4232c70f795d05937fc
Daniel Ortiz <danbedoya2001@gmail.com>
2025-04-04 14:10:03 -0500

    se corrigen tablas donde habian campos que no se generaron en la base de datos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,11 +11,9 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex rounded-md bg-green-600 hover:bg-green-700 focus:outline-none transition ease-in-out duration-150">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-white inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none transition ease-in-out duration-150">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
+                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-white inline-flex items-center px-3 py-2 border border-transparent hover:border-green-700 text-sm leading-4 font-medium rounded-md bg-green-600 hover:bg-green-700 focus:outline-none transition ease-in-out duration-150">
+                    {{ __('Dashboard') }}
+                </x-nav-link>
             </div>
 
             <!-- Settings Dropdown -->
