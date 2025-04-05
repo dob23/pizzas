02b21463a6e3f692c3810d70cb200869752de029
Daniel Ortiz <danbedoya2001@gmail.com>
2025-04-05 10:50:41 -0500

    proveedores

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,8 @@
 @php
-    $isSmallLogo = request()->routeIs('dashboard') || request()->routeIs('profile.edit');
+    $isSmallLogo = 
+    request()->routeIs('dashboard') || 
+    request()->routeIs('profile.edit') || 
+    request()->routeIs('proveedores.index');
 @endphp
 
 <img src="{{ asset('images/logo2.png') }}" alt="pizza_logo" 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProveedorController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//RUTAS DE PROVEEDORES
+Route::middleware('auth')->group(function(){
+    Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores.index');
 });
 
 require __DIR__.'/auth.php';
