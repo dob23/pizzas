ca0a9b0c43a46bcb72325c6bd8cdaecc5f63b179
Daniel Ortiz <danbedoya2001@gmail.com>
2025-04-06 15:55:25 -0500

    se crea modelo controlador y carpeta con index de compras

---
+++ resources/views/compras/index.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.4/jquery-confirm.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.4/jquery-confirm.min.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+
+    <title>Listado de Compras</title>
+  </head>
+  <body>
+    <x-app-layout>
+      <x-slot name="header">
+        <h2 class="font-semibold text-xl text-white leading-tight">
+          {{ __('Compras') }}
+        </h2>
+      </x-slot>
+      
+      <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+              <a href="{{ route('compras.index') }}" 
+                 class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded ml-1">
+                <i class="fa-solid fa-plus"></i> Nueva compra
+              </a>
+
+              <table class="table mt-3">
+                <thead>
+                  <tr>
+                    <th>Codigo</th>
+                    <th>Proveedor</th>
+                    <th>Materia Prima</th>
+                    <th>Cantidad</th>
+                    <th>Costo Total</th>
+                    <th>Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($compras as $compra)
+                    <tr>
+                        <td>{{ $compra->id }}</td>
+                        <td>{{ $compra->supplier_name }}</td>
+                        <td>{{ $compra->material_name }}</td>
+                        <td>{{ $compra->quantity }} {{ $compra->unit }}</td>
+                        <td>${{ $compra->total_cost }}</td>
+                        <td>
+                            <span>Acciones</span>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+              </table>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </x-app-layout>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    @vite(['resources/js/delete.js'])
+  </body>
+</html>
