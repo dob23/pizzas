29f18194855c82bd0fce1b4f1fcdf79fb7ffca94
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2025-04-06 11:30:54 -0500

    ingre mas pedido

--- app/Models/Ingrediente.php
+++ app/Models/Ingrediente.php
@@ -7,7 +7,7 @@ class Ingrediente extends Model
 {
     protected $fillable = ['name', 'price'];
 
-    // Relación con pedidos (a través de la tabla pivote)
+    
     public function pedidos()
     {
         return $this->belongsToMany(Pedido::class, 'code1_extra_trigedient_id', 'extra_trigedient_id', 'order_id')
--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    protected $table = 'codes'; // Nombre real de la tabla en DB
+    protected $table = 'codes'; 
 
     protected $fillable = [
         'client_id',
@@ -16,20 +16,20 @@ class Pedido extends Model
         'delivery_person_id'
     ];
 
-    // Relación con el usuario (cliente)
+    
     public function cliente()
     {
         return $this->belongsTo(User::class, 'client_id');
     }
 
-    // Relación con ingredientes extras (tabla pivote)
+    
     public function ingredientesExtras()
     {
         return $this->belongsToMany(Ingrediente::class, 'code1_extra_trigedient_id', 'order_id', 'extra_trigedient_id')
                    ->withPivot('quantity');
     }
 
-    // Relación con el cocinero/mensajero
+    
     public function repartidor()
     {
         return $this->belongsTo(Cocinero::class, 'delivery_person_id');
