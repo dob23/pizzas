fd609b4b7cab22d31c9e61d95f951e2dcf1f6599
Diana Perez <dianaperezc10@gmail.com>
2025-04-07 18:42:58 -0500

    se creo el modelo y el controlador :)

---
+++ app/Models/Empleado.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $table = 'employees';
+    public $timestamps = true;
+    protected $fillable = [
+        'user_id',
+        'position',
+        'identification_number',
+        'salary',
+        'hire_date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Orden::class, 'delivery_person_id');
+    }
+}
