51d36eab5ba7f76c5cb89921a7e9a47c26224f66
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2025-04-09 16:18:37 -0500

    Agregar imagen de el Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,21 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("🍕Bienvenido 👋🏻"). Auth::user()->name}}
+                <div class="p-4 text-gray-900">
+                    {{ __("🍕Bienvenido 👋🏻 "). Auth::user()->name }}
+                </div>
+
+                <!-- Imagen -->
+                <div class="p-6 flex justify-center">
+                    <img src="{{ asset('images/Familia pizza.jpg') }}" alt="Familia pizza" class="rounded-lg shadow-md max-w-full h-auto">
+                </div>
+
+                <!-- Footer personalizado -->
+                <div class="text-center text-sm text-gray-500 py-6">
+                    © 2025 Pizzería El Buen Sabor. Todos los derechos reservados.
                 </div>
             </div>
         </div>
