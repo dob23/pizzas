628396c15ea5c8b109b88aea0e3b219a3761a1be
Diana Perez <dianaperezct10@gmail.com>
2025-04-09 19:26:30 -0500

    se crea view de sucursal

--- app/Models/Sucursal.php
+++ app/Models/Sucursal.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sucursal extends Model
 {
-    //
+    protected $table = 'branches';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'email',
+        'manager_id',
+    ];
 }
